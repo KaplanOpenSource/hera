5d95377f9b0e4cd2acb29600c7a8e860cf2d4113
eyal <eyal@bb.gov.il>
2020-12-30 09:47:42 +0200

    added conversion of geopandas to DEM to topography file. also removed the inner functions that make an stl file from the topography class and created an stlFactory class, which is called by the topography analysis layer.

--- hera/measurements/GIS/locations/topography.py
+++ hera/measurements/GIS/locations/topography.py
@@ -1,18 +1,18 @@
 import logging
 from .abstractLocation import datalayer as locationDatalayer
-import matplotlib.pyplot as plt
 import geopandas
 from scipy.interpolate import griddata
-from numpy import array, cross, sqrt
 import numpy
 import pandas
-import math
 import random
 import gdal
 import numpy as np
 from .shapes import datalayer as shapeDatalayer
 from shapely.geometry import Point,box,MultiLineString, LineString
 import requests
+from unum.units import *
+import scipy
+from ..topography.STL import stlFactory
 
 
 class datalayer(locationDatalayer):
@@ -174,7 +174,8 @@ class analysis():
                                             gridyMin=[newdict["desc"]["yMin"]], gridyMax=[newdict["desc"]["yMax"]],
                                             gridzMin=[newdict["desc"]["zMin"]], gridzMax=[newdict["desc"]["zMax"]]))
         else:
-            stlstr, newdata = self.Convert_geopandas_to_stl(gpandas=geodata, points=points, flat=flat, NewFileName=NewFileName)
+            stl = stlFactory()
+            stlstr, newdata = stl.Convert_geopandas_to_stl(gpandas=geodata, points=points, flat=flat, NewFileName=NewFileName)
 
         if save:
             p = self.datalayer.FilesDirectory if path is None else path
@@ -191,165 +192,72 @@ class analysis():
                                                              dataFormat="string")
         return stlstr, newdata
 
-    def _make_facet_str(self, n, v1, v2, v3):
-        facet_str = 'facet normal ' + ' '.join(map(str, n)) + '\n'
-        facet_str += '  outer loop\n'
-        facet_str += '      vertex ' + ' '.join(map(str, v1)) + '\n'
-        facet_str += '      vertex ' + ' '.join(map(str, v2)) + '\n'
-        facet_str += '      vertex ' + ' '.join(map(str, v3)) + '\n'
-        facet_str += '  endloop\n'
-        facet_str += 'endfacet\n'
-        return facet_str
+    def getDEM(self,data, addToDB=False,**kwargs):
 
-    def _makestl(self, X, Y, elev, NewFileName):
-        """
-            Takes a mesh of x,y and elev and convert it to stl file.
+        if type(data) == str:
+            geodata = self.datalayer.getDocuments(CutName=data,**kwargs)[0].getData()
+        elif type(data) == geopandas.geodataframe.GeoDataFrame:
+            geodata = data
+        else:
+            raise KeyError("data should be geopandas dataframe or a string.")
+        xmin = geodata['geometry'].bounds['minx'].min()
+        xmax = geodata['geometry'].bounds['maxx'].max()
 
-            X - matrix of x coordinate. [[ like meshgrid ]]
-            Y - matrix of y coordinate. [[ like meshgrid ]]
-            elev - matrix of elevation.
+        ymin = geodata['geometry'].bounds['miny'].min()
+        ymax = geodata['geometry'].bounds['maxy'].max()
+        documents = self.datalayer.getMeasurementsDocuments(type="DEM", bounds=[xmin, ymin, xmax, ymax], dxdy=self._dxdy)
+        if len(documents) == 0:
+            Nx = int(((xmax - xmin) / self._dxdy))
+            Ny = int(((ymax - ymin) / self._dxdy))
 
-        """
-        base_elev = elev.min() - 10
-        stl_str = 'solid ' + NewFileName + '\n'
-        for i in range(elev.shape[0] - 1):
-            for j in range(elev.shape[1] - 1):
+            print("Mesh boundaries x=(%s,%s) ; y=(%s,%s); N=(%s,%s)" % (xmin, xmax, ymin, ymax, Nx, Ny))
+            dx = (xmax - xmin) / (Nx)
+            dy = (ymax - ymin) / (Ny)
+            print("Mesh increments: D=(%s,%s); N=(%s,%s)" % (dx, dy, Nx, Ny))
+            # 2.2 build the mesh.
+            grid_x, grid_y = numpy.mgrid[xmin:xmax:self._dxdy, ymin:ymax:self._dxdy]
+            # 3. Get the points from the geom
+            Height = []
+            XY = []
 
-                x = X[i, j];
-                y = Y[i, j]
-                v1 = [x, y, elev[i, j]]
-
-                x = X[i + 1, j];
-                y = Y[i, j]
-                v2 = [x, y, elev[i + 1, j]]
-
-                x = X[i, j];
-                y = Y[i, j + 1]
-                v3 = [x, y, elev[i, j + 1]]
-
-                x = X[i + 1, j + 1];
-                y = Y[i + 1, j + 1]
-                v4 = [x, y, elev[i + 1, j + 1]]
-
-                # dem facet 1
-                n = cross(array(v1) - array(v2), array(v1) - array(v3))
-                n = n / sqrt(sum(n ** 2))
-                stl_str += self._make_facet_str(n, v1, v2, v3)
-
-                # dem facet 2
-                n = cross(array(v2) - array(v3), array(v2) - array(v4))
-                n = n / sqrt(sum(n ** 2))
-                # stl_str += self._make_facet_str( n, v2, v3, v4 )
-                stl_str += self._make_facet_str(n, v2, v4, v3)
-
-                # base facets
-                v1b = list(v1)
-                v2b = list(v2)
-                v3b = list(v3)
-                v4b = list(v4)
-
-                v1b[-1] = base_elev
-                v2b[-1] = base_elev
-                v3b[-1] = base_elev
-                v4b[-1] = base_elev
-
-                n = [0.0, 0.0, -1.0]
-
-                stl_str += self._make_facet_str(n, v1b, v2b, v3b)
-                stl_str += self._make_facet_str(n, v2b, v3b, v4b)
-
-                vlist = [v1, v2, v3, v4]
-                vblist = [v1b, v2b, v3b, v4b]
-
-                # Now the walls.
-                for k, l in [(0, 1), (0, 2), (1, 3), (2, 3)]:
-                    # check if v[i],v[j] are on boundaries.
-                    kboundary = False
-                    if vlist[k][0] == X.min() or vlist[k][0] == X.max():
-                        kboundary = True
-
-                    lboundary = False
-                    if vlist[l][1] == Y.min() or vlist[l][1] == Y.max():
-                        lboundary = True
-
-                    if (kboundary or lboundary):
-                        # Add i,j,j-base.
-                        n = cross(array(vlist[k]) - array(vlist[l]), array(vblist[l]) - array(vlist[l]))
-                        n = n / sqrt(sum(n ** 2))
-                        stl_str += self._make_facet_str(n, vlist[k], vblist[l], vlist[l])
-
-                        # add j-base,i-base,i
-                        n = cross(array(vlist[k]) - array(vblist[k]), array(vlist[k]) - array(vblist[l]))
-                        n = n / sqrt(sum(n ** 2))
-                        stl_str += self._make_facet_str(n, vlist[k], vblist[k], vblist[l])
-
-        stl_str += 'endsolid ' + NewFileName + '\n'
-        return stl_str
-
-    def Convert_geopandas_to_stl(self, gpandas, points, NewFileName, dxdy=50, flat=None):
-        """
-            Gets a shape file of topography.
-            each contour line has property 'height'.
-            Converts it to equigrid xy mesh and then build the STL.
-        """
-
-        # 1. Convert contour map to regular height map.
-        # 1.1 get boundaries
-        xmin = points[0]
-        xmax = points[2]
-
-        ymin = points[1]
-        ymax = points[3]
-
-        print("Mesh boundaries x=(%s,%s) ; y=(%s,%s)" % (xmin, xmax, ymin, ymax))
-        # 1.2 build the mesh.
-        grid_x, grid_y = numpy.mgrid[(xmin):(xmax):dxdy, (ymin):(ymax):dxdy]
-        # 2. Get the points from the geom
-        Height = []
-        XY = []
-        for i, line in enumerate(gpandas.iterrows()):
-            if isinstance(line[1]['geometry'], LineString):
-                linecoords = [x for x in line[1]['geometry'].coords]
-                lineheight = [line[1]['HEIGHT']] * len(linecoords)
-                XY += linecoords
-                Height += lineheight
-            else:
-                for ll in line[1]['geometry']:
-                    linecoords = [x for x in ll.coords]
+            for i, line in enumerate(geodata.iterrows()):
+                if isinstance(line[1]['geometry'], LineString):
+                    linecoords = [x for x in line[1]['geometry'].coords]
                     lineheight = [line[1]['HEIGHT']] * len(linecoords)
                     XY += linecoords
                     Height += lineheight
-        if flat is not None:
-            for i in range(len(Height)):
-                Height[i] = flat
-        grid_z2 = griddata(XY, Height, (grid_x, grid_y), method='cubic')
-        grid_z2 = self.organizeGrid(grid_z2)
-        stlstr = self._makestl(grid_x, grid_y, grid_z2, NewFileName)
-
-        data = pandas.DataFrame({"XY": XY, "Height": Height, "gridxMin":grid_x.min(), "gridxMax":grid_x.max(),
-                                 "gridyMin":grid_y.min(), "gridyMax":grid_y.max(), "gridzMin":grid_z2[~numpy.isnan(grid_z2)].min(), "gridzMax":grid_z2[~numpy.isnan(grid_z2)].max(),})
-
-        return stlstr, data
-
-    def organizeGrid(self, grid):
-
-        for row in grid:
-            for i in range(len(row)):
-                if math.isnan(row[i]):
-                    pass
                 else:
-                    break
-            for n in range(i):
-                row[n] = row[i]
-            for i in reversed(range(len(row))):
-                if math.isnan(row[i]):
-                    pass
-                else:
-                    break
-            for n in range(len(row)-i):
-                row[-n-1] = row[i]
-        return grid
+                    for ll in line[1]['geometry']:
+                        linecoords = [x for x in ll.coords]
+                        lineheight = [line[1]['HEIGHT']] * len(linecoords)
+                        XY += linecoords
+                        Height += lineheight
 
+            grid_z2 = griddata(XY, Height, (grid_x, grid_y), method='cubic')
+
+            if np.ma.is_masked(grid_z2):
+                nans = grid_z2.mask
+            else:
+                nans = np.isnan(grid_z2)
+            notnans = np.logical_not(nans)
+            grid_z2[nans] = scipy.interpolate.griddata((grid_x[notnans], grid_y[notnans]), grid_z2[notnans],
+                                                 (grid_x[nans], grid_y[nans]), method='nearest').ravel()
+
+            DEMstring = f"{xmin} {xmax} {ymin} {ymax}\n{int(Nx)} {int(Ny)}\n"
+
+            for i in range(Nx):
+                for j in range(Ny):
+                    DEMstring += f"{(float(grid_z2[j, i]))} "
+                DEMstring += "\n"
+
+            if addToDB:
+                self.datalayer.addMeasurementsDocument(desc=dict(bounds=[xmin, ymin, xmax, ymax], dxdy=self._dxdy, **kwargs),
+                                                             type="DEM",
+                                                             resource=DEMstring,
+                                                             dataFormat="string")
+        else:
+            DEMstring = documents[0].getData()
+        return DEMstring
 
 def get_altitdue_ip(lat, lon):
     """
@@ -415,6 +323,7 @@ def get_altitdue_gdal(lat, lon):
     height = (1. - (rastery - int(rastery))) * height1 + (rastery - int(rastery)) * height2
 
     return height
+
 
 
 if __name__ == "__main__":
