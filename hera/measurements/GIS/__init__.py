7cd9874cc9fb88005b159a61fba93958df40c242
Omri Buchman <omrib@iibr.gov.il>
2021-06-07 09:57:52 +0300

    Updating units module for spyder

--- hera/measurements/GIS/__init__.py
+++ hera/measurements/GIS/__init__.py
@@ -1,9 +1,9 @@
-from .demography import datalayer as demographyDatalayer
-demography = demographyDatalayer("Demography")
+#from .demography import datalayer as demographyDatalayer
+#demography = demographyDatalayer("Demography")
 
-from .locations.image import datalayer as ImageDatalayer
-from .locations.topography import datalayer as TopographyDatalayer
-from .locations.buildings import datalayer as BuildingsDatalayer
-topography = TopographyDatalayer("Topography")
-buildings = BuildingsDatalayer("Buildings")
+#from .locations.image import datalayer as ImageDatalayer
+#from .locations.topography import datalayer as TopographyDatalayer
+#from .locations.buildings import datalayer as BuildingsDatalayer
+#topography = TopographyDatalayer("Topography")
+#buildings = BuildingsDatalayer("Buildings")
 #image = ImageDatalayer('imageLocations')
--- hera/simulations/utils/__init__.py
+++ hera/simulations/utils/__init__.py
@@ -1,6 +1,6 @@
 from unum import Unum
 from unum.units import *
-
+from unum import NameConflictError
 
 def toUnum(x,unit):
     return x.asUnit(unit) if isinstance(x,Unum) else x*unit
@@ -10,16 +10,18 @@ def toNumber(x,unit):
 
 
 ########################## Units for the model.
+try:
+	atm   = Unum.unit('atm',1.01325*bar,'atmosphere')
+	mbar  = Unum.unit('mbar',bar/1000,'millibar')
 
-atm   = Unum.unit('atm',1.01325*bar,'atmosphere')
-mbar  = Unum.unit('mbar',bar/1000,'millibar')
+	mmHg  = Unum.unit('mmHg',atm/760.,'mmHg = 1 torr')
+	torr  = Unum.unit('torr',atm/760.,'torr = 1 mmHg')
 
-mmHg  = Unum.unit('mmHg',atm/760.,'mmHg = 1 torr')
-torr  = Unum.unit('torr',atm/760.,'torr = 1 mmHg')
-
-dyne  = Unum.unit('dyne',1e-5*N,'dyne')
-poise = Unum.unit('poise',g/cm/s,'poise')
-cpoise = Unum.unit('cpoise',poise/10.,'centipoise')
+	dyne  = Unum.unit('dyne',1e-5*N,'dyne')
+	poise = Unum.unit('poise',g/cm/s,'poise')
+	cpoise = Unum.unit('cpoise',poise/10.,'centipoise')
+except NameConflictError:
+	pass
 
 ####
 
