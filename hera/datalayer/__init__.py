cdfb7b03a6bc268f6ca12c1c1f3ef6435bdae19d
ofir <ofirb@iibr.gov.il>
2021-07-06 11:39:13 +0300

    presentationLayer of riskAssessments: fixed a bug in the rose plot. LSM: changed the saving of simulations runs, saves the parameters in a dict called params. Also enabled using singleSimulation without "/netcdf".

--- hera/datalayer/__init__.py
+++ hera/datalayer/__init__.py
@@ -1,7 +1,7 @@
 from .document import getDBObject, createDBConnection, getMongoConfigFromJson,getDBNamesFromJSON
 from .collection import AbstractCollection,Measurements_Collection,Simulations_Collection,Cache_Collection
 from .project import getProjectList
-from .project import Project, ProjectMultiDB, ProjectMultiDBPublic
+from .project import Project
 from .document import nonDBMetadataFrame
 
 from ..utils.query import dictToMongoQuery
