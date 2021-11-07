f11e6143a1b3f929144f4c7d51887a9be0b03ad3
yehudaa <yehudaa@iibr.gov.il>
2021-11-07 18:19:01 +0200

    rewritting the lambda calculation interface in the building toolkit.

--- hera/simulations/openFoam/NavierStokes/canopyWindProfile.py
+++ hera/simulations/openFoam/NavierStokes/canopyWindProfile.py
@@ -35,31 +35,75 @@ class preProcess(toolkit.abstractToolkit):
             os.system("mkdir -p %s" % os.path.abspath(filesDirectory))
             self._FilesDirectory = filesDirectory
 
-    def windProfile(self, cellData, LambdaGrid,stations,saveMode=toolkit.TOOLKIT_SAVEMODE_FILEANDDB_REPLACE,file=None,**kwargs):
+    def windProfile(self, cellCenters, lambdaGrid, stations):
+        """
+            Creates a log-normal wind profile in the mesh.
+
+
+        Parameters
+        ----------
+        cellCenters: pandas.DataFrame
+                The cell centers in the grid in which the results should be calculated.
+                Has columns:
+                    - x
+                    - y
+                    - z
+
+        lambdaGrid: pandas.DataFrame, geopandas.GeoDataFrame.
+
+                The lambda of the urban grid.
+                Has the following fields:
+                    - lambda_f
+                    - lambda_p
+                    - hx
+
+                if GeoDataFrame: then take the x,y of the lambda from the centroid of the geometry.
+                Otherwise, they have to be supplied.
+
+                Calculates the Lc,ll,zz0, beta, and dd (displacenemnt).
+
+        stations: pandas.DataFrame
+                The wind speed and direction above building height
+
+                Has the columns:
+                    x
+                    y
+                    velocity
+                    direction (for now i is mathematical degrees).
+
+                If has 1 station - define it along the entire domain.
+                If has more than 1 station - interpolate each cell according to the inverse distance
+                (takes the height differences into account).
+
+        Returns
+        -------
+            pandas.DataFrame
+                columns U_x, U_y hold the velocity in each field.
+
+        """
 
         stations = stations.reset_index()
-        cellData = cellData.copy()
-        LambdaGrid = LambdaGrid.copy()
+        lambdaGrid = lambdaGrid.copy()
         aa_c = 0.25
         ccc = 1.0
 
         beta = 0.2
         norm_fac = 0.000000000000000000000000000001
 
-        LambdaGrid.loc[(LambdaGrid.hc<2),"lambda_f"]=0.25
-        LambdaGrid.loc[(LambdaGrid.hc < 2), "lambda_p"] = 0.25
-        LambdaGrid.loc[(LambdaGrid.hc < 2), "hc"] = 2
-        LambdaGrid.loc[(LambdaGrid.lambda_f > 0.4), "lambda_f"] = 0.4
-        LambdaGrid.loc[(LambdaGrid.lambda_p > 0.6), "lambda_f"] = 0.6
-        LambdaGrid["Lc"] = LambdaGrid["hc"] * (1 - LambdaGrid["lambda_p"]) / LambdaGrid["lambda_f"]
-        LambdaGrid["ll"] = 2 * (beta ** 3) * LambdaGrid["Lc"]
-        LambdaGrid["zz0"] = LambdaGrid["ll"] / 0.4 * numpy.exp(-0.4 / beta)
-        LambdaGrid["dd"] = LambdaGrid["ll"] / 0.4
-        if isinstance(LambdaGrid,geopandas.geodataframe.GeoDataFrame):
-            LambdaGrid["x"] = LambdaGrid.geometry.centroid.x
-            LambdaGrid["y"] = LambdaGrid.geometry.centroid.y
-        dx = LambdaGrid.reset_index()["x"][1]-LambdaGrid.reset_index()["x"][0]
-        dy = LambdaGrid.reset_index()["y"][1] - LambdaGrid.reset_index()["y"][0]
+        lambdaGrid.loc[(lambdaGrid.hc < 2), "lambda_f"]=0.25
+        lambdaGrid.loc[(lambdaGrid.hc < 2), "lambda_p"] = 0.25
+        lambdaGrid.loc[(lambdaGrid.hc < 2), "hc"] = 2
+        lambdaGrid.loc[(lambdaGrid.lambda_f > 0.4), "lambda_f"] = 0.4
+        lambdaGrid.loc[(lambdaGrid.lambda_p > 0.6), "lambda_f"] = 0.6
+        lambdaGrid["Lc"] = lambdaGrid["hc"] * (1 - lambdaGrid["lambda_p"]) / lambdaGrid["lambda_f"]
+        lambdaGrid["ll"] = 2 * (beta ** 3) * lambdaGrid["Lc"]
+        lambdaGrid["zz0"] = lambdaGrid["ll"] / 0.4 * numpy.exp(-0.4 / beta)
+        lambdaGrid["dd"] = lambdaGrid["ll"] / 0.4
+        if isinstance(lambdaGrid, geopandas.geodataframe.GeoDataFrame):
+            lambdaGrid["x"] = lambdaGrid.geometry.centroid.x
+            lambdaGrid["y"] = lambdaGrid.geometry.centroid.y
+        dx = lambdaGrid.reset_index()["x"][1] - lambdaGrid.reset_index()["x"][0]
+        dy = lambdaGrid.reset_index()["y"][1] - lambdaGrid.reset_index()["y"][0]
 
         pi = numpy.pi
         tgr = 300.0
@@ -72,32 +116,39 @@ class preProcess(toolkit.abstractToolkit):
         stations["temperature"] = tgr
 
         if len(stations)==1:
-            LambdaGrid["U2h"] = float(stations["velocity"].max())
-            LambdaGrid["theta"] = 270. - float(stations["direction"].max())
-            LambdaGrid["theta"] = LambdaGrid["theta"] * pi / 180.0
+            lambdaGrid["U2h"] = float(stations["velocity"].max())
+            lambdaGrid["theta"] = 270. - float(stations["direction"].max())
+            lambdaGrid["theta"] = lambdaGrid["theta"] * pi / 180.0
         else:
-            LambdaGrid["sum_u"] = 0.0
-            LambdaGrid["sum_v"] = 0.0
-            LambdaGrid["total_sum"] = 0.0
+            lambdaGrid["sum_u"] = 0.0
+            lambdaGrid["sum_v"] = 0.0
+            lambdaGrid["total_sum"] = 0.0
             for i in range(len(stations)):
-                LambdaGrid["r2"] = (LambdaGrid["x"] - stations["x"][i]) ** 2 + (LambdaGrid["y"] - stations["y"][i]) ** 2
-                LambdaGrid["z_diff_sqr"] = (2 * LambdaGrid["h_c"] - stations["height"][i]) ** 2
-                LambdaGrid["weight_h"] = 1. / (1. + LambdaGrid["r2"] / aa2)
-                LambdaGrid["weight_v"] = 1. / (1. + ccc * LambdaGrid["z_diff_sqr"] / aa2)
-                LambdaGrid["total_weight"] = LambdaGrid["weight_h"] * LambdaGrid["weight_v"]
-                LambdaGrid["sum_u"] += LambdaGrid["total_weight"] * stations["ust1"][i]
-                LambdaGrid["sum_v"] += LambdaGrid["total_weight"] * stations["vst1"][i]
-                LambdaGrid["total_sum"] += LambdaGrid["total_weight"]
+                lambdaGrid["r2"] = (lambdaGrid["x"] - stations["x"][i]) ** 2 + (lambdaGrid["y"] - stations["y"][i]) ** 2
+                lambdaGrid["z_diff_sqr"] = (2 * lambdaGrid["h_c"] - stations["height"][i]) ** 2
+                lambdaGrid["weight_h"] = 1. / (1. + lambdaGrid["r2"] / aa2)
+                lambdaGrid["weight_v"] = 1. / (1. + ccc * lambdaGrid["z_diff_sqr"] / aa2)
+                lambdaGrid["total_weight"] = lambdaGrid["weight_h"] * lambdaGrid["weight_v"]
+                lambdaGrid["sum_u"] += lambdaGrid["total_weight"] * stations["ust1"][i]
+                lambdaGrid["sum_v"] += lambdaGrid["total_weight"] * stations["vst1"][i]
+                lambdaGrid["total_sum"] += lambdaGrid["total_weight"]
 
-            LambdaGrid["uu"] = 1.0 * LambdaGrid["sum_u"] / (LambdaGrid["total_sum"] + norm_fac)
-            LambdaGrid["vv"] = 1.0 * LambdaGrid["sum_v"] / (LambdaGrid["total_sum"] + norm_fac)
-            LambdaGrid["U2h"] = numpy.sqrt((LambdaGrid["uu"]) ** 2 + (LambdaGrid["vv"]) ** 2)
-            LambdaGrid["theta"] = numpy.atan2(LambdaGrid["vv"], LambdaGrid["uu"])
+            lambdaGrid["uu"] = 1.0 * lambdaGrid["sum_u"] / (lambdaGrid["total_sum"] + norm_fac)
+            lambdaGrid["vv"] = 1.0 * lambdaGrid["sum_v"] / (lambdaGrid["total_sum"] + norm_fac)
+            lambdaGrid["U2h"] = numpy.sqrt((lambdaGrid["uu"]) ** 2 + (lambdaGrid["vv"]) ** 2)
+            lambdaGrid["theta"] = numpy.atan2(lambdaGrid["vv"], lambdaGrid["uu"])
 
-        LambdaGrid["Uh"] = (LambdaGrid["U2h"] * 0.4) / (beta * numpy.log((LambdaGrid["hc"] + LambdaGrid["dd"]) / LambdaGrid["zz0"]))
-        partLambdaGrid = pandas.DataFrame({"x":LambdaGrid["x"],"y":LambdaGrid["y"],"Uh":LambdaGrid["Uh"],"hc":LambdaGrid["hc"],
-                                           "ll":LambdaGrid["ll"],"dd":LambdaGrid["dd"],"zz0":LambdaGrid["zz0"],
-                                           "U2h":LambdaGrid["U2h"],"theta":LambdaGrid["theta"]})
+        lambdaGrid["Uh"] = (lambdaGrid["U2h"] * 0.4) / (beta * numpy.log((lambdaGrid["hc"] + lambdaGrid["dd"]) / lambdaGrid["zz0"]))
+        partLambdaGrid = pandas.DataFrame({"x":lambdaGrid["x"],
+                                           "y":lambdaGrid["y"],
+                                           "Uh":lambdaGrid["Uh"],
+                                           "hc":lambdaGrid["hc"],
+                                           "ll":lambdaGrid["ll"],
+                                           "dd":lambdaGrid["dd"],
+                                           "zz0":lambdaGrid["zz0"],
+                                           "U2h":lambdaGrid["U2h"],
+                                           "theta":lambdaGrid["theta"]})
+
         lambdaXarray = partLambdaGrid.set_index(["x","y"]).to_xarray()
         Uh = []
         hc = []
@@ -107,9 +158,9 @@ class preProcess(toolkit.abstractToolkit):
         U2h = []
         theta = []
         valuesDict = {}
-        for i in range(len(cellData)):
-            x = cellData.loc[i]["x"]
-            y = cellData.loc[i]["y"]
+        for i in range(len(cellCenters)):
+            x = cellCenters.loc[i]["x"]
+            y = cellCenters.loc[i]["y"]
             if x not in valuesDict.keys():
                 valuesDict[x] = {}
             if y in valuesDict[x].keys():
@@ -125,45 +176,21 @@ class preProcess(toolkit.abstractToolkit):
             if i%5000==0 and i>=5000:
                 print(f"finished {i} steps")
         for variableList, name in zip([Uh,hc,ll,dd,zz0,U2h,theta],["Uh","hc","ll","dd","zz0","U2h","theta"]):
-            cellData[name] = variableList
-        cellData["Umag"] = 0
+            cellCenters[name] = variableList
+        cellCenters["Umag"] = 0
 
-        cellData.loc[(cellData.z<2*cellData.hc),"Umag"]= (cellData.loc[cellData.z>cellData.hc].loc[cellData.z<2*cellData.hc].Uh * beta / 0.4) * \
-                                                                                    numpy.log(
-                                                                       1.0 * (cellData.loc[cellData.z>cellData.hc].loc[cellData.z<2*cellData.hc].z -
-                                                                              cellData.loc[cellData.z>cellData.hc].loc[cellData.z<2*cellData.hc].hc +
-                                                                              cellData.loc[cellData.z>cellData.hc].loc[cellData.z<2*cellData.hc].dd) /
-                                                                              cellData.loc[cellData.z>cellData.hc].loc[cellData.z<2*cellData.hc].zz0)
-        cellData.loc[(cellData.z<cellData.hc),"Umag"]= cellData.loc[cellData.z<cellData.hc].Uh * numpy.exp(
-                                                                beta * (cellData.loc[cellData.z<cellData.hc].z -
-                                                                cellData.loc[cellData.z<cellData.hc].hc) /cellData.loc[cellData.z<cellData.hc].ll)
-        cellData.loc[(cellData.z > 2*cellData.hc), "Umag"] = cellData.loc[cellData.z>2*cellData.hc].U2h
+        cellCenters.loc[(cellCenters.z < 2 * cellCenters.hc), "Umag"]= (cellCenters.loc[cellCenters.z > cellCenters.hc].loc[cellCenters.z < 2 * cellCenters.hc].Uh * beta / 0.4) * \
+                                                                       numpy.log(
+                                                                           1.0 * (cellCenters.loc[cellCenters.z > cellCenters.hc].loc[cellCenters.z < 2 * cellCenters.hc].z -
+                                                                                  cellCenters.loc[cellCenters.z > cellCenters.hc].loc[cellCenters.z < 2 * cellCenters.hc].hc +
+                                                                                  cellCenters.loc[cellCenters.z > cellCenters.hc].loc[cellCenters.z < 2 * cellCenters.hc].dd) /
+                                                                           cellCenters.loc[cellCenters.z > cellCenters.hc].loc[cellCenters.z < 2 * cellCenters.hc].zz0)
+        cellCenters.loc[(cellCenters.z < cellCenters.hc), "Umag"]= cellCenters.loc[cellCenters.z < cellCenters.hc].Uh * numpy.exp(
+            beta * (cellCenters.loc[cellCenters.z < cellCenters.hc].z -
+                    cellCenters.loc[cellCenters.z < cellCenters.hc].hc) / cellCenters.loc[cellCenters.z < cellCenters.hc].ll)
+        cellCenters.loc[(cellCenters.z > 2 * cellCenters.hc), "Umag"] = cellCenters.loc[cellCenters.z > 2 * cellCenters.hc].U2h
 
-        cellData["U_x"] = numpy.cos(cellData.theta) * cellData.Umag
-        cellData["U_y"] = numpy.sin(cellData.theta) * cellData.Umag
-        if saveMode in [toolkit.TOOLKIT_SAVEMODE_FILEANDDB,
-                        toolkit.TOOLKIT_SAVEMODE_FILEANDDB_REPLACE,
-                        toolkit.TOOLKIT_SAVEMODE_ONLYFILE,
-                        toolkit.TOOLKIT_SAVEMODE_ONLYFILE_REPLACE]:
+        cellCenters["U_x"] = numpy.cos(cellCenters.theta) * cellCenters.Umag
+        cellCenters["U_y"] = numpy.sin(cellCenters.theta) * cellCenters.Umag
 
-            file = os.path.join(self.FilesDirectory, "windProfile.parquet") if file is None else file
-
-            if os.path.exists(file) and saveMode in [toolkit.TOOLKIT_SAVEMODE_ONLYFILE,
-                                                     toolkit.TOOLKIT_SAVEMODE_FILEANDDB]:
-                raise FileExistsError(f"The output file {file} exists")
-
-            cellData.to_parquet(file, compression="gzip")
-
-            if saveMode in [toolkit.TOOLKIT_SAVEMODE_FILEANDDB_REPLACE,
-                            toolkit.TOOLKIT_SAVEMODE_FILEANDDB]:
-
-                regionDoc = self.getCacheDocuments(resource=file, dataFormat="parquet", type="windProfile",
-                                                             desc=kwargs)
-
-                if len(regionDoc) > 0 and saveMode == toolkit.TOOLKIT_SAVEMODE_FILEANDDB:
-                    raise ValueError(f"{file} already exists in the DB")
-                else:
-                    self.addCacheDocument(resource=file, dataFormat="parquet",
-                                                    type="windProfile", desc=kwargs)
-
-        return cellData
+        return cellCenters
