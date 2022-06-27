9599e8a53de53fa47ab62f4f28c49efac5729573
Shai Sussman <shai.su@kaplanopensource.co.il>
2022-06-27 09:10:22 +0300

    Added Unittest for datalyer parquet type
    Pandas dataframe is generated and a value is inserted,
    the dataframe is saved as parquet and metadata is stored in MongoDB
    
    Type of tests:
    1. check same value - inserted random int and retrieved
    
    2. check negative number - insrted random negative int.
    
    3.  check almost equal - inserted positive float
    and changed the value a bit, tested the value by a small epsilon, tested if
    retrieved value is within threshold
    
    4. test query count,
    created a number of parquets each with a different metadata
    tested number of retrieved resutls based on
    query search

---
+++ test_adding_data_parquet.py
@@ -0,0 +1,115 @@
+import json
+import os
+import pandas
+import numpy
+from scipy.stats import norm
+import  matplotlib.pyplot as plt 
+from hera import datalayer
+import unittest
+import warnings
+
+class TestAddingDataParquet(unittest.TestCase):
+    
+    def run_test_queries(self,y,projectName):
+        # logging.getLogger(__name__).disabled = True
+
+        x = numpy.linspace(norm.ppf(0.01), norm.ppf(0.99), 100)
+
+        dataset1 = pandas.DataFrame(dict(x=x,y=norm.pdf(x,loc=0,scale=1)))
+        dataset2 = pandas.DataFrame(dict(x=x,y=norm.pdf(x,loc=0,scale=0.5)))
+        dataset3 = pandas.DataFrame(dict(x=x,y=norm.pdf(x,loc=0.5,scale=0.5)))
+
+        workingdir = os.getcwd()
+        # print(f"The current file directory is {workingdir}")
+
+        dataset1File = os.path.join(workingdir,"dataset1.parquet")
+        dataset2File = os.path.join(workingdir,"dataset2.parquet")
+        dataset3File = os.path.join(workingdir,"dataset3.parquet")
+
+        dataset1.to_parquet(dataset1File,engine='fastparquet',compression='GZIP')
+        dataset2.to_parquet(dataset2File,engine='fastparquet',compression='GZIP')
+        dataset3.to_parquet(dataset3File,engine='fastparquet',compression='GZIP')
+
+
+        datalayer.Measurements.addDocument(projectName=projectName,
+                                        type="Distribution",
+                                        dataFormat=datalayer.datatypes.PARQUET,
+                                        resource=dataset1File,
+                                        desc=dict(loc=0,scale=1))
+
+        datalayer.Measurements.addDocument(projectName=projectName,
+                                        type="Distribution",
+                                        dataFormat=datalayer.datatypes.PARQUET,
+                                        resource=dataset2File,
+                                        desc=dict(loc=0,scale=0.5))
+
+        datalayer.Measurements.addDocument(projectName=projectName,
+                                        type="Distribution",
+                                        dataFormat=datalayer.datatypes.PARQUET,
+                                        resource=dataset3File,
+                                        desc=dict(loc=0.5,scale=0.5))
+        
+        # print(projectName)
+
+        List1 = datalayer.Measurements.getDocuments(projectName=projectName,loc=y)
+        return len(List1)
+
+
+    def insert_retrieve(self,testID,projectName,x):
+        warnings.simplefilter("ignore", ResourceWarning)
+
+        dataset = pandas.DataFrame(dict(x=[x]))
+
+        workingdir = os.getcwd()
+        # print(f"The current file directory is {workingdir}")
+
+        datasetFile = os.path.join(workingdir,f"{projectName}_{testID}.parquet")
+
+        dataset.to_parquet(datasetFile,engine='fastparquet',compression='GZIP')
+        datalayer.Measurements.addDocument(projectName=projectName,
+                                type="Distribution",
+                                dataFormat=datalayer.datatypes.PARQUET,
+                                resource=datasetFile,
+                                desc=dict(test_id = testID))
+
+        result = datalayer.Measurements.getDocuments(projectName=projectName,test_id=testID)
+        dataset_read = pandas.read_parquet(result[len(result)-1].resource)
+        result_val = dataset_read.loc[0,'x']
+        return result_val
+
+
+    # def test_query_count(self):
+    #     # The exact assertion should return 2, for the nubmer of returnred queries
+    #     returnedItems = self.run_test_queries(0,'test_1')
+    #     self.assertEqual(returnedItems,2)
+    
+
+    def test_equal_returned_values(self):
+        """
+        Generates a random number and returns if the number from dataset is the same
+        """
+        number = numpy.random.randint(0,10000)
+        result = self.insert_retrieve('test_exact_value','unittest_parquet',number)
+        self.assertTrue(result==number,'Return number not equal to given number')
+
+
+
+    def test_negative_nubmers(self):
+        """
+        The assertion should return True if the value is negative
+        """
+        number = numpy.random.randint(-1, -1000000)
+        result = self.insert_retrieve('test_negative_values','unittest_parquet',number)
+        self.assertLess(result,0,'Number not negative')
+
+    def test_almost_equal(self):
+        """
+        The assertion should return a number close to the generated number
+        """
+        number = numpy.random.uniform(0.1, 1000000)
+        number_close = number- 0.000001
+        result = self.insert_retrieve('test_close_values','unittest_parquet',number_close)
+        self.assertAlmostEqual(number, result,5,'Number not almost equal')
+
+if __name__ =='__main__':
+    unittest.main(warnings='ignore')
