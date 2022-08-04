0d077ffe20f3351702b17f37a875695c7f426c85
Shai Sussman <shai.su@kaplanopensource.co.il>
2022-08-04 21:57:31 +0300

    Modified Test_Datalayer to adopt the single/multiple differentiation

--- test_datalayer.py
+++ test_datalayer.py
@@ -1,8 +1,9 @@
 import unittest
-import hera_data_load
+from hera_data_load_unittest import loader
 import json
 import pathlib
 import os
+import warnings
 
 class Test_Datalayer(unittest.TestCase):
     def setUp(self):
@@ -10,14 +11,14 @@ class Test_Datalayer(unittest.TestCase):
         self.json_file = json.load(f)
         f.close()
     def test_update_metadata_and_retrieve(self):
-        projectName = 'Test Project'
+        warnings.simplefilter("ignore", ResourceWarning)
+        projectName = 'Test Project 1'
         for dataType in self.json_file['dataType'].keys():
             for source in self.json_file['dataType'][dataType].keys():
-                with self.subTest(f'{projectName} {dataType} {source}'):
+                with self.subTest(f'----------{projectName} {dataType} {source}---------'):
                     expectedResult = f"Added source {source} to tool {dataType} in project {projectName}"
                     foundResult  = f"Source {source} already exists in {projectName}"
-                    self.assertTrue(hera_data_load.load(projectName,dataType,source)==foundResult,expectedResult)
-    
+                    self.assertTrue(loader(projectName,dataType,source)==foundResult,expectedResult)   
 
 if __name__ == '__main__':
-    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+    unittest.main(argv=['first-arg-is-ignored'],warnings='ignore')
