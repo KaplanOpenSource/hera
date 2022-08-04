c8e375598db6faea21aad83f0535387f8dab645b
Shai Sussman <shai.su@kaplanopensource.co.il>
2022-08-04 20:58:10 +0300

    Add load json and iterator for unittest

--- test_datalayer.py
+++ test_datalayer.py
@@ -1,17 +1,23 @@
 import unittest
 import hera_data_load
+import json
+import pathlib
+import os
 
-# continue here
-#http://agiletesting.blogspot.com/2005/01/python-unit-testing-part-1-unittest.html
-
-class TestDatalayer(unittest.TestCase):
+class Test_Datalayer(unittest.TestCase):
+    def setUp(self):
+        f = open(os.path.join(pathlib.Path(__file__).parent,"datasources.json"))
+        self.json_file = json.load(f)
+        f.close()
     def test_update_metadata_and_retrieve(self):
-        source = "BNTL"
-        dataType = "GIS_Buildings"
-        projectName = 'testProject'
-        expectedResult = f"Added source {source} to tool {dataType} in project {projectName}"
-        foundResult  = f"Source {source} already exists in {projectName}"
-        self.assertTrue(hera_data_load.load(projectName,dataType,source)==foundResult,expectedResult)
+        projectName = 'Test Project'
+        for dataType in self.json_file['dataType'].keys():
+            for source in self.json_file['dataType'][dataType].keys():
+                with self.subTest(f'{projectName} {dataType} {source}'):
+                    expectedResult = f"Added source {source} to tool {dataType} in project {projectName}"
+                    foundResult  = f"Source {source} already exists in {projectName}"
+                    self.assertTrue(hera_data_load.load(projectName,dataType,source)==foundResult,expectedResult)
+    
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
