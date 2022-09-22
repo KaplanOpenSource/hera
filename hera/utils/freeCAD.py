9786069b1642af414d00e92f6e9ccba4fae0de33
yehudaa <yehudaa@iibr.gov.il>
2022-09-22 22:24:00 +0300

    Some fixes for the dispersion

---
+++ hera/utils/freeCAD.py
@@ -0,0 +1,38 @@
+import numpy
+try:
+    from freecad import app as FreeCAD
+    import Mesh
+except ImportError:
+    raise ImportError("freecad is not installed. please install it before trying again.")
+
+
+def getObjFileBoundaries(fileName):
+    """
+            Loads the file in FreeCAD and finds its boundaries.
+            Can work with anyfile freeCAD recognizes (stl,obj,...).
+    Parameters
+    ----------
+    fileName :  str
+
+    Returns
+    -------
+        dict
+            with keys:
+                - 'XMax', 'YMax', 'ZMax'
+                - 'XMin', 'YMin', 'ZMin'
+    """
+    Mesh.open(fileName)
+    objFile = FreeCAD.getDocument("Unnamed")
+
+    bboxes = [x.Mesh.BoundBox for x in objFile.findObjects()]
+
+    maxPropList = ['XMax', 'YMax', 'ZMax']
+    corners = dict()
+    for propName in maxPropList:
+        corners[propName] = numpy.max([getattr(x, propName) for x in bboxes]) / 1000
+
+    minPropList = ['XMin', 'YMin', 'ZMin']
+    for propName in minPropList:
+        corners[propName] = numpy.min([getattr(x, propName) for x in bboxes]) / 1000
+
+    return corners
