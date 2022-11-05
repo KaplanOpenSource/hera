e2e66c3c8de86bd92c1a29e2525fc4b6e1d9f339
yehudaa <yehuda.arav@gmail.com>
2022-11-05 22:51:01 +0200

    Upgrading the diff of the configurations to handle lists in jsons.
    It was done by building a general function to flatten the json to dataframe and to compare two datasets.

--- hera/utils/jsonutils.py
+++ hera/utils/jsonutils.py
@@ -1,9 +1,10 @@
 from unum.units import *
 import os
 import json
+import pandas
 from json.decoder import JSONDecodeError
 
-def ConvertJSONtoConf(JSON):
+def convertJSONtoConf(JSON):
     """
         Traverse the JSON and replace all the unum values with objects.
 
@@ -14,7 +15,7 @@ def ConvertJSONtoConf(JSON):
     for key,value in JSON.items():
         print(key,value)
         if isinstance(value,dict):
-            ret[key] = ConvertJSONtoConf(JSON[key])
+            ret[key] = convertJSONtoConf(JSON[key])
         elif isinstance(value,list):
             ret[key] = value
         else:
@@ -72,3 +73,134 @@ def loadJSON(jsonData):
 
     return  loadedjson
 
+
+def processJSONToPandas(jsonData, nameColumn="parameterName", valueColumn="value"):
+    """
+        Trnasforms a JSON to pandas, flattens a list items and names them according to their order
+
+        The example the JSON :
+        {
+          "nodes": {
+              "a" : {
+                "x" : 1,
+                "y" : 2,
+                "z" : 3
+              },
+               "b" : {
+                "r" : 1,
+                "t" : 2,
+                "y" : [3,2,4,5,6]
+              }
+          }
+        }
+
+        will be converted to
+
+        parameterName  value
+        --------------------
+    0     nodes.a.x     1
+    1     nodes.a.y     2
+    2     nodes.a.z     3
+    3     nodes.b.r     1
+    4     nodes.b.t     2
+    5   nodes.b.y_0     3
+    6   nodes.b.y_1     2
+    7   nodes.b.y_2     4
+    8   nodes.b.y_3     5
+    9   nodes.b.y_4     6
+
+        Notes:
+
+            - Currently does not support JSON whose root is a list.
+              [
+                {  "a" : 1 },
+                {  "b" : 2}
+
+              ]
+                It will be supported if needed in the future.
+
+        Parameters
+        ----------
+
+        jsonData : dict
+            the JSON data (a dict)
+
+        nameColumn: str
+            The name of the parameter column name
+
+        valueColumn : str
+            The name of the value
+
+    """
+
+    pnds = pandas.json_normalize(jsonData).T.reset_index().rename(columns={'index': nameColumn, 0: valueColumn})\
+        .explode(valueColumn)\
+        .reset_index()
+
+    listParameters = pnds.groupby(nameColumn).count().query(f"{valueColumn}>1").index
+
+    for pname in listParameters:
+        counter = 0
+        for I, dta in pnds.iterrows():
+            if dta.loc[nameColumn] == pname:
+                pnds.loc[I, nameColumn] = f"{pname}_{counter}"
+                counter += 1
+
+    return pnds[[nameColumn,valueColumn]]
+
+
+def convertJSONtoPandas(jsonData, nameColumn="parameterName", valueColumn="value"):
+    """
+        converts a JSON (either in file or loaded, or json str) to pandas.
+        The pandas flattens the JSON using the json path convection.
+        e.g
+        {
+            "a" : {
+                "b" : 1,
+                "c" : [1,2,3]
+            }
+        }
+
+        will be converted to
+         a.b  1
+         a.c_0 1
+         a.c_1 2
+         a.c_2 3
+
+
+        Does not support (currently) JSON whose root is a list but only supports dict
+
+    Parameters
+    ----------
+    jsonData : str,dict
+        A json data either a file name, a json dict string, or a dict.
+
+
+        nameColumn: str
+            The name of the parameter column name
+
+        valueColumn : str
+            The name of the value
+
+    Returns
+    -------
+            pandas.DataFrame
+
+            with the fields nameColumn (the path of the json) and valueColumn
+    """
+    param1 =  loadJSON(jsonData)
+    pnds1 = processJSONToPandas(param1,nameColumn=nameColumn,valueColumn=valueColumn)
+    dictIndex = pnds1.apply(lambda x: isinstance(x.value,dict),axis=1)
+    while dictIndex.sum()>0:
+        base = [pnds1[~dictIndex]]
+
+        toProcessList = pnds1[dictIndex].set_index("parameterName")[['value']]
+        for pname,data in toProcessList.iterrows():
+            newdata = processJSONToPandas(data.value,nameColumn=nameColumn,valueColumn=valueColumn)
+            newdata = newdata.assign(parameterName=newdata.parameterName.apply(lambda x: f"{pname}.{x}"))
+            base.append(newdata)
+
+        param1 = pandas.concat(base,ignore_index=True)
+        dictIndex = param1.apply(lambda x: isinstance(x.value, dict), axis=1)
+
+    return param1
