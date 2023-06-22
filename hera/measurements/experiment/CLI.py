f862391c460fd3df15b83dc9e1e44f621eee9fdb
yehudaa <yehuda.arav@gmail.com>
2023-06-22 07:11:36 +0300

    Fixing the CLI module name

--- hera/measurements/experiment/CLI.py
+++ hera/measurements/experiment/CLI.py
@@ -1,1 +1,14 @@
+from ...uti
+
 def experiments_list(arguments):
+    logger = logging.getLogger("hera.bin")
+    logger.execution(f"----- Start -----")
+    logger.debug(f" arguments: {arguments}")
+
+    if 'projectName' not in arguments:
+        configurationFile = arguments.configurationFile if 'configurationFile'  in arguments else "caseConfiguration.json"
+
+        configuration = loadJSON(configurationFile)
+        projectName = configuration['projectName']
+    else:
+        projectName = arguments.projectName
