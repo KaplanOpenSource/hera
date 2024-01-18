845c61bfb9d6475e4f17648bab24e5ae39e295f1
yehudaa <yehudaa@iibr.gov.il>
2024-01-18 17:26:30 +0200

    Updates to the CLI and the openFOAM toolkit (especially the dispersion).

--- hera/__init__.py
+++ hera/__init__.py
@@ -18,6 +18,8 @@ from .toolkit import ToolkitHome
 from .datalayer import Project
 toolkitHome = ToolkitHome()
 """
+    #108: Updating the openFOAM CLI. 
+    
 
     #110:   Updating the datalayer documentation and converting it to Jupyter-lab
             Moving the config from the toolkit to the project. 
