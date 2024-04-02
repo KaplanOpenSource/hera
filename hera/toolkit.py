95d3ef74bb7be7da55b4a9d4c33e84622409d1f6
Elia Salo <eliasalo@example.com>
2024-04-02 15:55:36 +0300

    ExperimentToolKit documentation version 1.

--- hera/toolkit.py
+++ hera/toolkit.py
@@ -449,7 +449,7 @@ class abstractToolkit(Project):
                 delargs = {TOOLKIT_DATASOURCE_NAME : dataSourceName,
                            TOOLKIT_DATASOURCE_VERSION :  version}
 
-                self.deleteDataSourceDocuments(**delargs)
+                self.deleteDataSource(**delargs)
             #else:
                 # print("Does not exist: add data source.")
 
