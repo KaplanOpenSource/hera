f3251c6f48f5b977944c1baef130a60e24ddf9f2
Elia Salo <eliasalo@example.com>
2024-04-02 11:38:21 +0300

    ExperimentToolKit documentation version 1.

--- hera/utils/data/toolkit.py
+++ hera/utils/data/toolkit.py
@@ -99,7 +99,7 @@ class dataToolkit(toolkit.abstractToolkit):
 
                     theItem = itemDesc["item"]
                     isRelativePath = itemDesc.get("isRelativePath",True)
-                    logger.debug(f"Checking if {itemName} resource is a path {isPath}, is it absolute? {isAbsolute}")
+                    logger.debug(f"Checking if {itemName} resource is a path {isRelativePath}, is it absolute? {isAbsolute}")
 
                     if isRelativePath:
                         logger.debug(f"The input is not absolute (it is relative). Adding the path {basedir} to the resource {theItem['resource']}")
