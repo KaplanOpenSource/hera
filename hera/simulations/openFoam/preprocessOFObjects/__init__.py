7346c5050e95435a4f4eaedbf43b030799660bf2
yehudaa <yehudaa@iibr.gov.il>
2024-07-04 12:46:42 +0300

    Updaties to the OF objects.

---
+++ hera/simulations/openFoam/preprocessOFObjects/__init__.py
@@ -0,0 +1,6 @@
+from .OFObject import OFObject
+from .OFList import OFList
+from .OFObjectHome import OFObjectHome
+from .OFMeshBoundary import OFMeshBoundary
+from .OFField import OFField
+from .utils import extractFieldFile,ParsedParameterFileToDataFrame
