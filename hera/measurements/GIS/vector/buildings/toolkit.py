f8c26d8bacbcf451457a82ebbe8ad0f3c94049e3
Elia Salo <eliasalo@example.com>
2024-08-15 14:01:37 +0300

    GIS Buiilding toolkit updated + Documentation and Sphinx

--- hera/measurements/GIS/vector/buildings/toolkit.py
+++ hera/measurements/GIS/vector/buildings/toolkit.py
@@ -70,6 +70,7 @@ class BuildingsToolkit(VectorToolkit):
         Parameters
         ----------
         buildingData : geopandas.DataFrame
+            The buildings data.
 
         buildingHeightColumn : string
             The name of the column that holds the height of the buildings in [m].
@@ -78,13 +79,13 @@ class BuildingsToolkit(VectorToolkit):
             The name of the column that holds the elevation of the building.
 
         outputFileName : string
-            The absolute path of the output STL
+            The absolute path of the output STL.
 
         flatTerrain : bool
             If true, use a flat terrain.
 
         nonFlatTopographyShift : float
-            shift the house with respect to its height in the topography.
+            Shift the house with respect to its height in the topography.
 
         referenceTopography : float [default 0]
             If flatTerrain, use this as the reference height for the buildings.
@@ -186,136 +187,3 @@ class BuildingsToolkit(VectorToolkit):
             buildings = self.getBuildingHeightFromRasterTopographyToolkit(buildings)
 
         return buildings
-
-    def regionToSTL(self, regionNameOrData, outputFileName, dataSourceName, flat=None, saveMode=None, crs=None):
-        """
-            Converts the document to the stl and saves it to the disk.
-            Adds the stl file to the DB.
-
-            Parameters
-            ----------
-            regionNameOrData: str or geopandas .
-                The name of the datasource or the geopandas file to convert.
-                If geopandas has the following columns:
-
-            dataSourceName: string
-                The name of the datasource that contains the database of the buildings
-
-            flat: None or float.
-                The base of the building.
-                If None, use the buildings height.
-
-            outputfile: str
-                a path to the output file.
-
-            saveMode: str
-                - None, does not add to the DB.
-                - TOOLKIT_SAVEMODE_FILEANDDB_REPLACE replace the document if exists
-                - TOOLKIT_SAVEMODE_FILEANDDB         throws exception.
-
-            crs : int [optional]
-                Used if the CRS of the shapeData is different than the CRS of the datasource.
-
-            Returns
-            -------
-                The maximal height
-
-        """
-        self.logger.info("---- Start ----")
-
-        if not isinstance(regionNameOrData, geopandas.GeoDataFrame):
-            shp = self.cutRegionFromSource(regionNameOrData, datasourceName=dataSourceName, isBounds=True, crs=crs)
-        else:
-            shp = regionNameOrData
-
-        self.logger.info(f"Region {regionNameOrData} consists of {len(shp)} buildings")
-
-        maxheight = self.buildingsGeopandasToSTL(buildingData=shp, outputFileName=outputFileName, flat=flat)
-
-        if saveMode in [TOOLKIT_SAVEMODE_FILEANDDB_REPLACE,
-                        TOOLKIT_SAVEMODE_FILEANDDB]:
-
-            regionNameSTL = outputFileName.split(".")[0] if "." in outputFileName else outputFileName
-
-            doc = self.getSTL(regionNameSTL)
-
-            if doc is not None and saveMode == TOOLKIT_SAVEMODE_FILEANDDB:
-                raise ValueError(f"STL {regionNameSTL} exists in project {self.projectName}")
-
-            desc = {
-                toolkit.TOOLKIT_VECTOR_REGIONNAME: regionNameSTL,
-                toolkit.toolkitExtension.TOOLKIT_TOOLKITNAME_FIELD: self.toolkitName
-            }
-
-            if doc is None:
-                self.addCacheDocument(type=self.doctype,
-                                      resource=os.path.abspath(outputFileName),
-                                      dataFormat=self.datatypes.STRING,
-                                      desc=desc)
-            else:
-                doc.resource = os.path.abspath(outputFileName)
-                doc.desc = desc
-                doc.save()
-
-        return maxheight
-
-    def buildingsToSTL(self, point1, point2, domainname, outputfile):
-        """
-            write stl file of the buildings in a domain
-
-        Parameters
-        ----------
-        point1 lower left corner (ITM)
-        point2 upper right corner
-        domainname - domain name
-        outputfile - where to write the file
-
-        Returns
-        -------
-
-        How to use
-        ----------
-        from hera.measurements.GIS.vector.buildings.toolkit import BuildingsToolkit
-        BuildingsToolkit.buildingstorToSTL([200000, 740000], [201000, 741000], 'test', 'test2.stl')
-
-        What to fix
-        -----------
-
-        """
-        # print("poinat1")
-        bounding = [point1[0], point1[1], point2[0], point2[1]]
-        # bounding = point1
-
-        self.addRegion(bounding, domainname, crs=2039)
-        reg = self.cutRegionFromSource(domainname, datasourceName='BNTL', isBounds=True, crs=2039)
-
-        bt.regionToSTL(domainname, outputfile, 'BNTL')
-        return
-
-    if __name__ == "__main__":
-        fig, ax = plt.subplots()
-        bt = BuildingsToolkit('kaplan', filesDirectory='/home/hadas/Hadas_S/Kaplan/')
-        reg = [177933, 663923, 178933, 664423]
-        # reg = bt.cutRegionFromSource(reg, 'BNTL', True)
-        # reg.to_file('/home/hadas/Hadas_S/Kaplan/BNTL.shp')
-        reg.plot(ax=ax)
-        bt.addRegion(reg, 'buildings', crs=2039)
-        # reg =[177933,663923,178933,664923]
-        # lis = vt.getRegionNameList()
-        reg = bt.cutRegionFromSource('new9', dataSourceName='BNTL', isBounds=True, crs=2039)
-        data = gps.GeoDataFrame.from_file(
-            '/mnt/public/omri_hadas/Production_Mode/Dispersion_Model/Haifa09_aerosols/LSM_for_SOURCE_ESTIMATION_epsilon_version/Lambda_Inputs/Haifa_Krayot_202323_741796/290_rez_200_afterBLD_correction/BLD_krayot_after_correction.shp')
-        lm = bt.analysis.LambdaOfDomain(270, 200, buildingsDataSourceNameOrData=data, crs=2039)
-        lm = bt.LambdaFromBuildingData(270, 200, data)
-        # lm = bt._analysis.LambdaFromDatasource(270, 200, 'test', exteriorBlockNameOrData=reg, crs=2039)
-        # p=1
-
-    # newSketch = FreeCADDOC.addObject('Sketcher::SketchObject', 'Sketch3180')
-    # newSketch.Placement = FreeCAD.Placement(FreeCAD.Vector(0.000000, 0.000000, 19.86), FreeCAD.Rotation(0.000000, 0.000000, 0.000000, 1.000000))
-    # newSketch.addGeometry(Part.LineSegment(FreeCAD.Vector(179062.00002653955, 662887.8099938995, 19.86),FreeCAD.Vector(179028.70002653956, 662904.1199938995, 19.86)))
-    # newSketch.addGeometry(Part.LineSegment(FreeCAD.Vector(179028.70002653956, 662904.1199938995, 19.86),FreeCAD.Vector(179038.50002653955, 662923.9999938995, 19.86)))
-    # newSketch.addGeometry(Part.LineSegment(FreeCAD.Vector(179038.50002653955, 662923.9999938995, 19.86),FreeCAD.Vector(179071.80002653957, 662907.6199938995, 19.86)))
-    # newSketch.addGeometry(Part.LineSegment(FreeCAD.Vector(179071.80002653957, 662907.6199938995, 19.86),FreeCAD.Vector(179062.00002653955, 662887.8099938995, 19.86)))
-    # FreeCADDOC.addObject('Part::Extrusion', 'building3180')
-    # newPad.Base = newSketch
-    # newPad.LengthFwd = 21.62
