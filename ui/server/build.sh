7137f86cf345d80542292dc95f1209201c687716
Eran Geva <eran@kaplanopensource.co.il>
2025-09-30 18:34:52 +0200

    fix docker ui server

--- ui/server/build.sh
+++ ui/server/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Building base image..."
-docker build -t hera-base -f Dockerfile .
+# echo "Building base image..."
+# docker build -t hera-base -f Dockerfile .
 
 echo "Building server image..."
-docker build -t hera-server -f server/Dockerfile .
+docker build -t hera-server -f Dockerfile .
 
-echo "Build complete."
+echo "\nBuild complete.\n\nTo run server use:"
+echo "docker run -it -p 8000:8000 --rm --name hera-server-instance hera-server"
