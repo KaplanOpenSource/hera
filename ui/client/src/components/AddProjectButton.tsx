be305ce7ec5832281d3d60d13bc61de43fb02fb2
Eran Geva <eran@kaplanopensource.co.il>
2025-10-28 18:52:31 +0100

    dialog for adding project

--- ui/client/src/components/AddProjectButton.tsx
+++ ui/client/src/components/AddProjectButton.tsx
@@ -1,17 +1,16 @@
-import { Add } from "@mui/icons-material"
-import { IconButton } from "@mui/material"
+import { Add } from "@mui/icons-material";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, TextField } from "@mui/material";
+import { useState } from "react";
 import { execPython } from "../io/execPython";
-import { fetchProjectsNames, fetchProjectDetails } from "../io/FetchProjects";
+import { fetchProjectDetails, fetchProjectsNames } from "../io/FetchProjects";
 import { useProjectStore } from "../stores/useProjectStore";
 
 export const AddProjectButton = ({ }) => {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState('');
   const { selectProject } = useProjectStore();
 
   const addProject = async () => {
-    const name = prompt('New project name?');
-    if (!name) {
-      return;
-    }
     const { problem } = await execPython(`
 from types import SimpleNamespace
 from hera.utils.data.CLI import project_create
@@ -27,9 +26,39 @@ project_create(SimpleNamespace(projectName="${name}", directory=None, loadReposi
 
   return (<>
     <IconButton
-      onClick={() => addProject()}
+      onClick={() => setOpen(true)}
     >
       <Add />
     </IconButton>
+    <Dialog open={open} onClose={() => setOpen(false)}>
+      <DialogTitle>New Project</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Adding a new project, please fill initial details
+        </DialogContentText>
+        <TextField
+          autoFocus
+          required
+          margin="dense"
+          size="small"
+          label="Project Name"
+          fullWidth
+          variant="outlined"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setOpen(false)}>
+          Cancel
+        </Button>
+        <Button onClick={() => {
+          addProject();
+          setOpen(false);
+        }}>
+          Add Project
+        </Button>
+      </DialogActions>
+    </Dialog>
   </>)
 }
