fee7b2f5e567ebb618def5ce049887902cfa7d28
Eran Geva <eran@kaplanopensource.co.il>
2025-10-28 13:10:29 +0100

    building hera-server from project root, not from ui/server

--- ui/server/build.sh
+++ ui/server/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
+cd "$(dirname "$(dirname "$(dirname "$0")")")" && echo curr dir: $(pwd)
 
 # echo "Building base image..."
 # docker build -t hera-base -f Dockerfile .
 
 echo "Building server image..."
-docker build -t hera-server -f Dockerfile .
+docker build -t hera-server -f ui/server/Dockerfile-server .
 
 echo "\nBuild complete.\n\nTo run server use:"
 echo "docker run -it -p 8000:8000 --rm --name hera-server-instance hera-server"
