dcd0c68de975d48a549745e629c35ea0e0f32d8d
Eran Geva <eran@kaplanopensource.co.il>
2025-12-09 14:54:46 +0200

    RichTreeView flat  on DetailsViewDocument

--- ui/client/src/components/details/DetailsViewDocument.tsx
+++ ui/client/src/components/details/DetailsViewDocument.tsx
@@ -1,9 +1,27 @@
+import { RichTreeView } from '@mui/x-tree-view';
+import { TreeViewBaseItem } from '@mui/x-tree-view/models';
+
 export const DetailsViewDocument = ({
   doc,
 }: {
   doc: any,
 }) => {
-  return (<>
-    <pre>{JSON.stringify(doc, null, 2)}</pre>
-  </>)
+  const name = doc?.desc?.datasourceName || doc?.type || doc._cls;
+
+  const convert = (s: any) => {
+    const ret: TreeViewBaseItem[] = [];
+    for (const [k, v] of Object.entries(s)) {
+      ret.push({ id: k, label: k, children: [{ id: k + '_ch', label: JSON.stringify(v) }] });
+    }
+    return ret;
+  }
+  const obj: TreeViewBaseItem = {
+    id: '____rooot____',
+    label: name,
+    children: convert(doc)
+  };
+
+  return (<RichTreeView
+    items={[obj]}
+  />)
 }
