eb14958ea502d50987b96fd856ef27df24d16244
Eran Geva <eran@kaplanopensource.co.il>
2025-12-09 17:26:15 +0200

    convert to SimpleTreeView for customizations

--- ui/client/src/components/details/DetailsViewDocument.tsx
+++ ui/client/src/components/details/DetailsViewDocument.tsx
@@ -1,5 +1,42 @@
-import { RichTreeView } from '@mui/x-tree-view';
-import { TreeViewBaseItem } from '@mui/x-tree-view/models';
+import { SimpleTreeView, TreeItem } from '@mui/x-tree-view';
+
+const DetailsViewItem = ({
+  itemKey,
+  itemValue,
+  level = 0,
+  index,
+}: {
+  itemKey: string,
+  itemValue: any,
+  level: number,
+  index: number,
+}) => {
+  const key = `___lvl${level}_idx${index}_${itemKey}`
+  return (
+    <TreeItem
+      key={key}
+      itemId={key}
+      label={itemKey}
+    >
+      {typeof itemValue === 'object'
+        ? Object.entries(itemValue).map(([k, v], i) => (
+          <DetailsViewItem
+            key={k}
+            itemKey={k}
+            itemValue={v}
+            level={level + 1}
+            index={i}
+          />
+        ))
+        : (
+          <span>
+            {JSON.stringify(itemValue)}
+          </span>
+        )}
+    </TreeItem>
+
+  )
+}
 
 export const DetailsViewDocument = ({
   doc,
@@ -7,22 +44,16 @@ export const DetailsViewDocument = ({
   doc: any,
 }) => {
   const name = doc?.desc?.datasourceName || doc?.type || doc._cls;
+  const items: [string, any][] = Object.entries(doc);
 
-  const convert = (s: any) => {
-    const ret: TreeViewBaseItem[] = [];
-    for (const [k, v] of Object.entries(s)) {
-      const children = typeof v === 'object' ? convert(v) : [{ id: k + '_ch', label: JSON.stringify(v) }];
-      ret.push({ id: k, label: k, children });
-    }
-    return ret;
-  }
-  const obj: TreeViewBaseItem = {
-    id: '____rooot____',
-    label: name,
-    children: convert(doc)
-  };
-
-  return (<RichTreeView
-    items={[obj]}
-  />)
+  return (
+    <SimpleTreeView>
+      <DetailsViewItem
+        itemKey={name}
+        itemValue={doc}
+        level={0}
+        index={0}
+      />
+    </SimpleTreeView>
+  )
 }
