e93626c02267e61bc95c632538dadfc1f2a085b9
Eran Geva <eran@kaplanopensource.co.il>
2025-12-10 17:15:34 +0200

    moved updateDocument up to DetailsViewPanel

--- ui/client/src/components/details/DetailsViewDocument.tsx
+++ ui/client/src/components/details/DetailsViewDocument.tsx
@@ -1,12 +1,8 @@
 import { Done } from '@mui/icons-material';
 import { SimpleTreeView } from '@mui/x-tree-view';
-import { ProjectDocument } from '@shared/types';
 import { useState } from 'react';
 import { ButtonTooltip } from '../../elements/ButtonTooltip';
-import { execPython } from '../../io/execPython';
 import { DetailsViewItem } from './DetailsViewItem';
-
-const FORBIDDEN_FIELDS = ['_id', '_cls', 'projectName'];
 
 export const DetailsViewDocument = ({
   doc,
@@ -17,29 +13,6 @@ export const DetailsViewDocument = ({
 }) => {
   const [shownDoc, setShownDoc] = useState<any>(JSON.parse(JSON.stringify(doc)));
   const name = doc?.desc?.datasourceName || doc?.type || doc._cls;
-
-  const updateDocument = async () => {
-    const docid = (doc as ProjectDocument)._id.$oid;
-    const lines = [`
-from hera.datalayer import All
-doc = All.getDocumentByID('${docid}')
-`];
-    for (const [field, prevVal] of Object.entries(doc)) {
-      if (!FORBIDDEN_FIELDS.includes(field) && JSON.stringify(prevVal) !== JSON.stringify(shownDoc[field])) {
-        lines.push(`doc.${field} = ${JSON.stringify(shownDoc[field])}`)
-      }
-    }
-    lines.push(`
-doc.save()
-docs = All.getDocumentByID('${docid}')
-result = docs.asDict(with_id=True)
-`)
-    const code = lines.join('\n');
-    const { data } = await execPython(code);
-    if (data) {
-      setDoc(data)
-    }
-  }
 
   return (
     <SimpleTreeView>
@@ -53,7 +26,7 @@ result = docs.asDict(with_id=True)
           {JSON.stringify(doc) === JSON.stringify(shownDoc) ? null : (
             <ButtonTooltip
               title='Update Document'
-              onClick={updateDocument}
+              onClick={()=> setDoc(shownDoc)}
             >
               <Done />
             </ButtonTooltip>
--- ui/client/src/components/details/DetailsViewPanel.tsx
+++ ui/client/src/components/details/DetailsViewPanel.tsx
@@ -2,9 +2,11 @@ import { Paper } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { execPython } from '../../io/execPython';
 import { idFromDocId } from '../../shared/idDocId';
-import type { ProjectEntire } from '../../shared/types';
+import type { ProjectDocument, ProjectEntire } from '../../shared/types';
 import { DetailsViewDocument } from './DetailsViewDocument';
 import { DetailsViewProject } from './DetailsViewProject';
+
+const FORBIDDEN_FIELDS = ['_id', '_cls', 'projectName'];
 
 export const DetailsViewPanel = ({
   project,
@@ -34,13 +36,37 @@ result = docs.asDict(with_id=True)
     })()
   }, [docid])
 
+
+  const updateDocument = async (shownDoc: any) => {
+    const docid = (doc as ProjectDocument)._id.$oid;
+    const lines = [`
+from hera.datalayer import All
+doc = All.getDocumentByID('${docid}')
+`];
+    for (const [field, prevVal] of Object.entries(doc)) {
+      if (!FORBIDDEN_FIELDS.includes(field) && JSON.stringify(prevVal) !== JSON.stringify(shownDoc[field])) {
+        lines.push(`doc.${field} = ${JSON.stringify(shownDoc[field])}`)
+      }
+    }
+    lines.push(`
+doc.save()
+docs = All.getDocumentByID('${docid}')
+result = docs.asDict(with_id=True)
+`)
+    const code = lines.join('\n');
+    const { data } = await execPython(code);
+    if (data) {
+      setDoc(data)
+    }
+  }
+
   return (
     <Paper sx={{ p: 2, height: '100%' }}>
       {doc
         ? (
           <DetailsViewDocument
             doc={doc}
-            setDoc={setDoc}
+            setDoc={(newVal) => updateDocument(newVal)}
           />
         )
         : (
