bb318f26617c69f9c306ca16ed5e9d2b2de0d607
Eran Geva <eran@kaplanopensource.co.il>
2025-12-10 12:05:45 +0200

    DetailsViewItemSingle separate file

---
+++ ui/client/src/components/details/DetailsViewItemSingle.tsx
@@ -0,0 +1,47 @@
+import { FormControl, InputLabel, MenuItem, Select, Stack, TextField, Typography } from '@mui/material';
+import { Case, SwitchCase } from '../../elements/SwitchCase';
+import { useServerConstants } from '../../stores/useServerConstants';
+
+export const DetailsViewItemSingle = ({
+  itemKey,
+  itemValue,
+}: {
+  itemKey: string,
+  itemValue: any,
+}) => {
+  const { dataTypes } = useServerConstants();
+  return (
+    <Stack direction='row' spacing={1} justifyItems={'center'} alignItems={'center'}>
+      <Typography>
+        {itemKey}
+      </Typography>
+      <SwitchCase test={itemKey}>
+        <Case isDefault>
+          <TextField
+            size='small'
+            value={
+              JSON.stringify(itemValue)
+            }
+          />
+        </Case>
+        <Case value={'dataFormat'}>
+          <FormControl style={{ marginTop: 10, minWidth: '100px' }}>
+            <InputLabel id="demo-simple-select-label">{itemKey}</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={itemValue}
+              label="dataFormat"
+              size='small'
+            // onChange={(event: SelectChangeEvent) => setAge(event.target.value as string)}
+            >
+              {Object.entries(dataTypes).map(([_upcasename, name]) => (
+                <MenuItem key={name} value={name}>{name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Case>
+      </SwitchCase>
+    </Stack>
+  )
+}
