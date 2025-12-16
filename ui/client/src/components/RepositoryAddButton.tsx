8071918d1963e092c37353d471b1ae14c520756d
Eran Geva <eran@kaplanopensource.co.il>
2025-12-16 18:53:21 +0200

    fix click on repo json text

--- ui/client/src/components/RepositoryAddButton.tsx
+++ ui/client/src/components/RepositoryAddButton.tsx
@@ -57,13 +57,17 @@ result = {"project": project}
                 label="Repository Json (as string)"
                 fullWidth
                 value={values.repositoryJson}
+                onClick={e => e.stopPropagation()}
                 onChange={(e) => setValues({ ...values, repositoryJson: e.target.value })}
                 size="small"
+                rows={10}
+                multiline={true}
               />
               <TextField
                 label="Base Directory"
                 fullWidth
                 value={values.baseDir}
+                onClick={e => e.stopPropagation()}
                 onChange={(e) => setValues({ ...values, baseDir: e.target.value })}
                 size="small"
               />
