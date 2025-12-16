6fe101d585c9df564488cf856ff2c438f24f3ab0
Eran Geva <eran@kaplanopensource.co.il>
2025-12-16 19:24:43 +0200

    show checkbox for data sources

--- ui/client/src/components/repo/RepoAddEditor.tsx
+++ ui/client/src/components/repo/RepoAddEditor.tsx
@@ -4,7 +4,9 @@ import {
   FormGroup,
   Stack,
   TextField,
+  Typography,
 } from "@mui/material"
+import { useState } from "react";
 
 export type RepoAddParams = {
   repositoryJson: string;
@@ -12,6 +14,13 @@ export type RepoAddParams = {
   overwrite: boolean;
 };
 
+const parseJson = (str: string) => {
+  try {
+    return JSON.parse(str);
+  } catch {
+    return {}
+  }
+}
 export const RepoAddEditor = ({
   values,
   setValues,
@@ -19,18 +28,38 @@ export const RepoAddEditor = ({
   values: RepoAddParams,
   setValues: (newValues: RepoAddParams) => void,
 }) => {
+  const [sourcesUsed, setSourcesUsed] = useState<string[]>([]);
+  const jsonobj = parseJson(values.repositoryJson);
+  const dataSources = Object.keys(jsonobj);
   return (
     <Stack direction={'column'} spacing={2}>
-      <TextField
-        label="Repository Json (as string)"
-        fullWidth
-        value={values.repositoryJson}
-        onClick={e => e.stopPropagation()}
-        onChange={(e) => setValues({ ...values, repositoryJson: e.target.value })}
-        size="small"
-        rows={10}
-        multiline={true}
-      />
+      <Stack direction={'row'} spacing={2}>
+        <TextField
+          label="Repository Json (as string)"
+          value={values.repositoryJson}
+          onClick={e => e.stopPropagation()}
+          onChange={(e) => setValues({ ...values, repositoryJson: e.target.value })}
+          size="small"
+          rows={10}
+          multiline={true}
+        />
+        <Stack>
+          <Typography>
+            DataSources
+          </Typography>
+          {dataSources.map(d => (
+            <FormGroup key={d}>
+              <FormControlLabel
+                label={d}
+                control={<Checkbox
+                  checked={values.overwrite}
+                  onChange={(e) => setValues({ ...values, overwrite: e.target.checked })}
+                />}
+              />
+            </FormGroup>
+          ))}
+        </Stack>
+      </Stack>
       <TextField
         label="Base Directory"
         fullWidth
