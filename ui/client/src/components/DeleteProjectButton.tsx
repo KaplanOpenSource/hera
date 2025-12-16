beb50451e3faebc79190323e94233855cfe03fd2
Eran Geva <eran@kaplanopensource.co.il>
2025-12-16 11:49:24 +0200

    delete project

--- ui/client/src/components/DeleteProjectButton.tsx
+++ ui/client/src/components/DeleteProjectButton.tsx
@@ -2,6 +2,7 @@ import { Add, Delete } from "@mui/icons-material";
 import { ButtonTooltip } from "../elements/ButtonTooltip";
 import { useConfirm } from "../elements/useConfirm";
 import { useProjectStore } from "../stores/useProjectStore";
+import { execPython } from "../io/execPython";
 
 export const DeleteProjectButton = ({ }) => {
   const { confirmOpen, ConfirmDialog } = useConfirm()
@@ -19,7 +20,14 @@ export const DeleteProjectButton = ({ }) => {
           textValidate: (text) => text === currProjectName,
         });
         if (confirmed && text === currProjectName) {
-          // console.l
+          await execPython(`
+import json
+from hera.datalayer import All
+docs = All.getDocumentsAsDict('${currProjectName}', with_id=True)['documents']
+docs = sorted(docs, key=lambda d: d['type'] == '${currProjectName}__config__')
+for d in docs:
+    All.deleteDocumentByID(d['_id']['$oid'])
+            `);
         }
       }}
     >
