fb996642a4c6ada2db3e2bfbac5f0d5dc5a47d3e
Eran Geva <eran@kaplanopensource.co.il>
2025-12-16 13:05:47 +0200

    reorg DeleteProjectButton

--- ui/client/src/components/DeleteProjectButton.tsx
+++ ui/client/src/components/DeleteProjectButton.tsx
@@ -9,19 +9,8 @@ export const DeleteProjectButton = ({ }) => {
   const { confirmOpen, ConfirmDialog } = useConfirm()
   const { currProjectName, selectProject, setProjectNames, setCurrentProject } = useProjectStore();
 
-  return (<>
-    <ButtonTooltip
-      title='Delete project'
-      onClick={async () => {
-        const { confirmed, text } = await confirmOpen({
-          title: `Are you sure you want to delete ${currProjectName}?`,
-          requireText: true,
-          textLabel: 'Type project name to confirm',
-          textPlaceholder: currProjectName,
-          textValidate: (text) => text === currProjectName,
-        });
-        if (confirmed && text === currProjectName) {
-          const { data } = await execPython(`
+  const deleteProject = async () => {
+    const { data } = await execPython(`
 import json
 from hera.datalayer import All
 from hera.datalayer.project import getProjectList
@@ -37,11 +26,26 @@ project = {"name": projectNames[0]['name'], "documents": docs['documents']}
 
 result = {"projectNames": projectNames, "project": project}
             `);
-          const details = (data?.project) as ProjectEntire;
-          const names = (data?.projectNames || []) as ProjectName[];
-          setProjectNames(names);
-          setCurrentProject(details);
-          selectProject(details.name);
+    const details = (data?.project) as ProjectEntire;
+    const names = (data?.projectNames || []) as ProjectName[];
+    setProjectNames(names);
+    setCurrentProject(details);
+    selectProject(details.name);
+  }
+
+  return (<>
+    <ButtonTooltip
+      title='Delete project'
+      onClick={async () => {
+        const { confirmed, text } = await confirmOpen({
+          title: `Are you sure you want to delete ${currProjectName}?`,
+          requireText: true,
+          textLabel: 'Type project name to confirm',
+          textPlaceholder: currProjectName,
+          textValidate: (text) => text === currProjectName,
+        });
+        if (confirmed && text === currProjectName) {
+          await deleteProject();
         }
       }}
     >
