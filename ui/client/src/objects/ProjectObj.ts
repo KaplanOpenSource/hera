499cc0d7152ce9f8530b418fc32d0f8aa306957e
Eran Geva <eran@kaplanopensource.co.il>
2025-12-23 15:03:27 +0200

    properties on ProjectObj

--- ui/client/src/objects/ProjectObj.ts
+++ ui/client/src/objects/ProjectObj.ts
@@ -4,19 +4,22 @@ export class ProjectObj {
   constructor(
     public data: ProjectEntire,
   ) {
-
   }
 
-  documents(withoutConfig = true): DocumentObj[] {
-    let docs = this.data.documents.map(d => new DocumentObj(d, this));
-    if (withoutConfig) {
-      docs = docs.filter(d => !d.isConfig());
-    }
-    return docs;
+  public get name(): string {
+    return this.data.name;
   }
 
-  configDocument(): DocumentObj | undefined {
-    return this.documents().filter(d => d.isConfig)[0];
+  public get allDocuments(): DocumentObj[] {
+    return this.data.documents.map(d => new DocumentObj(d, this));
+  }
+
+  public get documents(): DocumentObj[] {
+    return this.allDocuments.filter(d => !d.isConfig);
+  }
+
+  public get configDocument(): DocumentObj | undefined {
+    return this.allDocuments.filter(d => d.isConfig)[0];
   }
 }
 
@@ -25,10 +28,18 @@ export class DocumentObj {
     public data: ProjectDocument,
     public project: ProjectObj,
   ) {
-
   }
 
-  isConfig(): boolean {
+  public get docid(): string {
+    return this.data._id.$oid;
+  }
+
+  public get isConfig(): boolean {
     return this.data.type === this.project.data.name + '__config__';
+  }
+
+  public get name(): string {
+    return this.data.desc?.datasourceName || this.data.type || this.data._cls;
+
   }
 }
