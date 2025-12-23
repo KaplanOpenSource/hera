5f275dd260aae942b6248adaf49f96bccc09778e
yehudaa <yehuda.arav@gmail.com>
2025-12-23 22:18:56 +0200

    A problem in jsonutils.

--- hera/utils/jsonutils.py
+++ hera/utils/jsonutils.py
@@ -503,7 +503,6 @@ class JSONvariationItem:
 
         result = {}
         for key, value in self.variationItem.items():
-            if isinstance(quantity, pint.Quantity):
                 result[key] = value[self._curIter]
         self._curIter += 1
         return result
