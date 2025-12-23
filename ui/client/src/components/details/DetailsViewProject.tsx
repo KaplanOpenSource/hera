8cd24ab6553030937267e9617a8aaa56582269c4
Eran Geva <eran@kaplanopensource.co.il>
2025-12-23 15:44:42 +0200

    showing config on project details

--- ui/client/src/components/details/DetailsViewProject.tsx
+++ ui/client/src/components/details/DetailsViewProject.tsx
@@ -1,15 +1,28 @@
 import { Article, ReceiptLong } from "@mui/icons-material"
 import { Stack, Typography } from "@mui/material"
 import { ProjectObj } from "../../objects/ProjectObj"
+import { DetailsViewDocument } from "./DetailsViewDocument"
 
 export const DetailsViewProject = ({
   project,
 }: {
   project: ProjectObj,
 }) => {
+  const doc = project.configDocument;
   return (<>
     <Typography variant="h6">{project.name}</Typography>
     {/* <Typography>ID: {project.name}</Typography> */}
+    {doc ? <>
+      <DetailsViewDocument
+        doc={doc.data}
+        setDoc={(newVal) => {
+          // changeDocument(newVal)
+        }}
+      />
+    </>
+      : <Typography>
+        No config document
+      </Typography>}
     <Typography>Documents: {project.documents?.length ?? 'N/A'}</Typography>
     {project.allDocuments?.map(d => (
       <Stack key={d.docid} direction='row'>
