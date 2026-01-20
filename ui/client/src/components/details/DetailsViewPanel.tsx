3e95f290b8f2bb84b9f806c8d5a40b984328b0d5
Eran Geva <eran@kaplanopensource.co.il>
2026-01-20 12:08:43 +0200

    reorg

--- ui/client/src/components/details/DetailsViewPanel.tsx
+++ ui/client/src/components/details/DetailsViewPanel.tsx
@@ -1,9 +1,6 @@
-import { Paper } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { fetchDocument, updateDocument } from '../../io/FetchDocument';
-import { DocumentObj, ProjectObj } from '../../objects/ProjectObj';
+import { ProjectObj } from '../../objects/ProjectObj';
 import { idFromDocId } from '../../shared/idDocId';
-import { DetailsViewDocument } from './DetailsViewDocument';
+import { DetailsViewDocId } from './DetailsViewDocId';
 
 export const DetailsViewPanel = ({
   project,
@@ -12,8 +9,6 @@ export const DetailsViewPanel = ({
   project: ProjectObj,
   showItemId: string,
 }) => {
-  const [doc, setDoc] = useState<any>(undefined);
-
   const docid = idFromDocId(showItemId);
   if (docid) {
     return (
@@ -36,46 +31,3 @@ export const DetailsViewPanel = ({
   }
 };
 
-
-export const DetailsViewDocId = ({
-  project,
-  docid,
-}: {
-  project: ProjectObj,
-  docid: string,
-}) => {
-  const [doc, setDoc] = useState<any>(undefined);
-
-  useEffect(() => {
-    (async () => {
-      if (docid) {
-        const data = await fetchDocument(docid);
-        if (data) {
-          setDoc(data);
-          return;
-        }
-      }
-      setDoc(undefined);
-    })()
-  }, [docid, project?.name])
-
-  const changeDocument = async (shownDoc: any) => {
-    const data = await updateDocument(shownDoc, doc);
-    if (data) {
-      setDoc(data)
-    }
-  }
-
-  return (
-    <>
-      {doc
-        ? (
-          <DetailsViewDocument
-            doc={new DocumentObj(doc, project)}
-            setDoc={(newDoc) => changeDocument(newDoc.data)}
-          />
-        )
-        : null}
-    </>
-  );
-};
