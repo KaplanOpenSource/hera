1615e0f1011552e43dc8e02c6e299c15b3bbc08d
Eran Geva <eran@kaplanopensource.co.il>
2026-03-16 19:33:22 -0100

    preview geojson

--- ui/client/src/components/details/PreviewChooser.tsx
+++ ui/client/src/components/details/PreviewChooser.tsx
@@ -1,9 +1,10 @@
 import { ProjectDocument } from '../../shared/types';
 import { ImagePreview, isImageResource } from './ImagePreview';
+import { GeoJsonPreview, isGeoJsonResource } from './maps/GeoJsonPreview';
 import { isTileUrl, TileMapView } from './maps/TileMapView';
 
 export const hasPreview = (doc: ProjectDocument): boolean => {
-  return isTileUrl(doc.resource) || isImageResource(doc.resource);
+  return isTileUrl(doc.resource) || isImageResource(doc.resource) || isGeoJsonResource(doc.resource);
 };
 
 export const PreviewChooser = ({
@@ -13,6 +14,9 @@ export const PreviewChooser = ({
 }) => {
   if (isTileUrl(doc.resource)) {
     return <TileMapView url={doc.resource} />;
+  }
+  if (isGeoJsonResource(doc.resource)) {
+    return <GeoJsonPreview path={doc.resource} />;
   }
   if (isImageResource(doc.resource)) {
     return <ImagePreview path={doc.resource} />;
