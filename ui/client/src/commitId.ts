6defcb2947d4dc92b3505a72115f95a6905a04f5
Eran Geva <eran@kaplanopensource.co.il>
2026-04-14 14:56:33 +0200

    rebuild

--- ui/client/src/commitId.ts
+++ ui/client/src/commitId.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const commitId = '5b5fefdbc7e6e7d4b5ed44f5587ab943e112c204';
-=======
-export const commitId = 'a71c0137d98ca64450f2862083db29299c64218a';
->>>>>>> master
+export const commitId = '35f4522827d5f08f869c0b748d7a45905e327166';
