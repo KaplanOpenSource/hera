6a44c411d9ca258b1af1b78d120ce6eac3a1ae76
Eran Geva <eran@kaplanopensource.co.il>
2026-06-03 14:06:10 +0200

    icon

--- ui/client/src/components/header/PageTitle.tsx
+++ ui/client/src/components/header/PageTitle.tsx
@@ -1,9 +1,12 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 export const PageTitle = () => {
   return (
-    <Typography variant="h4" gutterBottom sx={{ marginRight: 2 }}>
-      Hera UI
-    </Typography>
+    <>
+      <Box component="img" src="/atom.svg" alt="" sx={{ width: 32, height: 32, display: 'block', alignSelf: 'flex-start' }} />
+      <Typography variant="h4" gutterBottom sx={{ marginRight: 2 }}>
+        Hera UI
+      </Typography>
+    </>
   );
 };
